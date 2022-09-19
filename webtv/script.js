6d4ba9630bc1ead46c16d6672eed82d52c00c88b
Patrik <79414111+patrik-ks17@users.noreply.github.com>
2022-09-19 11:13:32 +0200

    Add files via upload

---
+++ webtv/script.js
@@ -0,0 +1,7 @@
+function videoplayer(x)
+ {
+	if(x==1) video.src="https://www.youtube.com/embed/TM2HBJ8e-XA?autoplay=1&rel=0"
+	if(x==2) video.src="https://www.youtube.com/embed/O7SZf3VnNkE?autoplay=1&rel=0"
+	if(x==3) video.src="https://www.youtube.com/embed/23IVh5v9hMk?autoplay=1&rel=0"
+ }
+
